7bd59679840063aa92c18af107d8732b84368b4d
Kjell Heinsen <ich@kjell-heinsen.de>
2025-07-20 21:59:56 +0200

    Arbeit an Implementationsklasse

--- u_securevault.pas
+++ u_securevault.pas
@@ -11,6 +11,24 @@ Definiert Schnittstelle zur SecureVault.dll
 
 interface
 
+uses
+  Winapi.Windows, System.SysUtils;
+
+  const
+  SECUREVAULT_DLL = 'SecureVault.dll';
+
+
+
+  // Wrapper-Klasse für einfache Verwendung
+type
+  TSecureVault = class
+  private
+
+  public
+
+  end;
+
+
 implementation
 
 end.
